5b5d169ae70b7d72666afa5dc26e444ceadc704d
Sharnam <sharnam34@gmail.com>
2024-04-04 08:01:49 +0545

    configs update

--- src/app/(routes)/work/[workSlug]/page.jsx
+++ src/app/(routes)/work/[workSlug]/page.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 import WorkDetail from "./workDetail";
+import workData from "@/app/data/work";
+
+export function generateStaticParams() {
+  const workSlugs = workData.map((work) => ({
+    workSlug: work.slug,
+  }));
+
+  return workSlugs;
+}
 
 export default function page() {
   return (
