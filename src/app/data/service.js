f0fd34cca30449f1a0593274aae13214d525f543
Sharnam <sharnam34@gmail.com>
2024-04-15 17:38:05 +0545

    data update

--- src/app/data/service.js
+++ src/app/data/service.js
@@ -56,21 +56,99 @@ export const serviceData = [
   {
     id: 2,
     slug: "ai-implementation",
-    title: "AI Implementation",
-    brief: " Superb design, flawless website—setting the internet's benchmark.",
+    title: "Web Development",
+    brief: "Superb design, flawless website—setting the internet's benchmark.",
     images: {
-      default: "/services/ai-implementation.png",
-      hover: "/services/ai-implementation-light.png",
+      default: "/services/web-dev-logo.png",
+      hover: "/services/web-dev-logo-light.png",
+    },
+    serviceDetail: {
+      heroTitle: "Desktop to Mobile Ready Web Design",
+      heroImage: "/services/web-development/web-dev-hero.png",
+      serviceBrief:
+        "At Beamlab we specialize in delivering cutting-edge web development services tailored to meet the unique needs of businesses and organizations across various industries. Our team of experienced developers and designers is dedicated to crafting exceptional digital experiences that drive results. Explore our range of services below:",
+      offerings: [
+        "Static and Dynamic Website",
+        "Complete Responsive Design",
+        "Optimum User Experience Design",
+        "SEO Optimization",
+        "Website Maintenance and Support ",
+      ],
+      sellingProposition: [
+        {
+          title: "Fast Performance",
+          description:
+            "This section is designed to visually and interactively demonstrate the speed and performance advantages of your services.  Lorem ipsum is a very long lasting example of random words being displayed .",
+          image: boltAnimation,
+        },
+        {
+          title: "Complete Responsiveness",
+          description:
+            "This section is designed to visually and interactively demonstrate the speed and performance advantages of your services.  Lorem ipsum is a very long lasting example of random words being displayed .",
+          image: responsiveAnimation,
+        },
+        {
+          title: "Secured webiste",
+          description:
+            "This section is designed to visually and interactively demonstrate the speed and performance advantages of your services.  Lorem ipsum is a very long lasting example of random words being displayed .",
+          image: securityAnimation,
+        },
+        {
+          title: "Custom Design",
+          description:
+            "This section is designed to visually and interactively demonstrate the speed and performance advantages of your services.  Lorem ipsum is a very long lasting example of random words being displayed .",
+          image: customDesignAnimation,
+        },
+      ],
     },
   },
   {
-    id: 3,
+    id: 1,
     slug: "mobile-app-development",
-    title: "Mobile App Development",
-    brief: " Superb design, flawless website—setting the internet's benchmark.",
+    title: "Web Development",
+    brief: "Superb design, flawless website—setting the internet's benchmark.",
     images: {
-      default: "/services/mobile-app-dev.png",
-      hover: "/services/mobile-app-dev-light.png",
+      default: "/services/web-dev-logo.png",
+      hover: "/services/web-dev-logo-light.png",
+    },
+    serviceDetail: {
+      heroTitle: "Desktop to Mobile Ready Web Design",
+      heroImage: "/services/web-development/web-dev-hero.png",
+      serviceBrief:
+        "At Beamlab we specialize in delivering cutting-edge web development services tailored to meet the unique needs of businesses and organizations across various industries. Our team of experienced developers and designers is dedicated to crafting exceptional digital experiences that drive results. Explore our range of services below:",
+      offerings: [
+        "Static and Dynamic Website",
+        "Complete Responsive Design",
+        "Optimum User Experience Design",
+        "SEO Optimization",
+        "Website Maintenance and Support ",
+      ],
+      sellingProposition: [
+        {
+          title: "Fast Performance",
+          description:
+            "This section is designed to visually and interactively demonstrate the speed and performance advantages of your services.  Lorem ipsum is a very long lasting example of random words being displayed .",
+          image: boltAnimation,
+        },
+        {
+          title: "Complete Responsiveness",
+          description:
+            "This section is designed to visually and interactively demonstrate the speed and performance advantages of your services.  Lorem ipsum is a very long lasting example of random words being displayed .",
+          image: responsiveAnimation,
+        },
+        {
+          title: "Secured webiste",
+          description:
+            "This section is designed to visually and interactively demonstrate the speed and performance advantages of your services.  Lorem ipsum is a very long lasting example of random words being displayed .",
+          image: securityAnimation,
+        },
+        {
+          title: "Custom Design",
+          description:
+            "This section is designed to visually and interactively demonstrate the speed and performance advantages of your services.  Lorem ipsum is a very long lasting example of random words being displayed .",
+          image: customDesignAnimation,
+        },
+      ],
     },
   },
 ];
