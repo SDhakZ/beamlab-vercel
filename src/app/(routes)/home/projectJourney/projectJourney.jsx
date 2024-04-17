0240359ee805d5c5c913984d8d5fe5ce55e55a69
Sharnam <sharnam34@gmail.com>
2024-04-17 16:21:53 +0545

    project journey structure complete

--- src/app/(routes)/home/projectJourney/projectJourney.jsx
+++ src/app/(routes)/home/projectJourney/projectJourney.jsx
@@ -1,6 +1,82 @@
-import React from "react";
+"use client";
+import React, { useRef, useState, useEffect } from "react";
 import "./PJ.css";
+import { projectJourneyData } from "@/app/data/projectJourney";
 
 export default function projectJourney() {
-  return <div className="min-h-screen margin-t padding-y">projectJourney</div>;
+  const pjRef = useRef(null);
+  const [activeItem, setActiveItem] = useState(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            setActiveItem(entry.target.getAttribute("data-id"));
+          }
+        });
+      },
+      {
+        root: null, // observing intersections relative to the viewport
+        threshold: 0.5, // trigger when 50% of the item is in the viewport
+      }
+    );
+
+    // Observe each item
+    document.querySelectorAll(".project-image").forEach((item) => {
+      observer.observe(item);
+    });
+
+    return () => observer.disconnect();
+  }, []);
+
+  return (
+    <section ref={pjRef} className="relative py-32 bg-background-black">
+      <h2 className="text-5xl text-center mb-36 text-white-shade-200">
+        Project Journey
+      </h2>
+
+      <div className="relative flex flex-col min-h-screen gap-8 sm:flex-row sm:gap-12 md:gap-16 lg:gap-20 xl:gap-24 justify-evenly container-margin">
+        <div className="sticky-image-container">
+          <ul className="flex flex-col gap-10 text-4xl text-white-shade-200">
+            {projectJourneyData.map((item) => (
+              <li
+                key={item.id}
+                className={
+                  activeItem === item.id.toString()
+                    ? "text-white"
+                    : "text-black-shade-100"
+                }
+              >
+                <span
+                  className={` ${
+                    activeItem === item.id.toString()
+                      ? "text-primary-orange-300"
+                      : "text-black-shade-100"
+                  } text-base `}
+                >
+                  &#40;0{item.id}&#41;
+                </span>{" "}
+                {item.name}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="flex gap-14 flex-col md:gap-16 sm:gap-24 lg:gap-28 xl:gap-36 sm:max-w-[280px] w-full md:max-w-[300px] lg:max-w-[370px] xl:max-w-[460px]">
+          {projectJourneyData.map((item) => (
+            <div
+              key={item.id}
+              className="flex flex-col gap-4 project-image"
+              data-id={item.id}
+            >
+              <img src={item.image} alt="" />
+              <p className="font-medium sm:text-base md:text-lg text-white-shade-200">
+                {item.description}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 }
---
+++ src/app/data/projectJourney.js
@@ -0,0 +1,38 @@
+export const projectJourneyData = [
+  {
+    id: 1,
+    name: "Requirement Collection",
+    image: "/project_journey/requirement-collection.png",
+    description:
+      "imagelorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in  ",
+  },
+
+  {
+    id: 2,
+    name: "Initial Designing",
+    image: "/project_journey/initial-design.png",
+    description:
+      "imagelorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in  ",
+  },
+  {
+    id: 3,
+    name: "Product Development",
+    image: "/partners/TechCrunch.png",
+    description:
+      "imagelorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in  ",
+  },
+  {
+    id: 4,
+    name: "Pre Launch",
+    image: "/partners/TheBoringCompany.png",
+    description:
+      "imagelorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in  ",
+  },
+  {
+    id: 5,
+    name: "Deploy Product",
+    image: "/partners/Hubspot.png",
+    description:
+      "imagelorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in  ",
+  },
+];
